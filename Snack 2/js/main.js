618fa45a5a967d5a34a51c4122e284a6784f09dd
Simone Viglione <zimoviglio51@gmail.com>
2023-03-10 16:04:16 +0100

    snack 2 generati punti e falli randomici

---
+++ Snack 2/js/main.js
@@ -0,0 +1,18 @@
+const squadre = [
+  { nome: "Quinzano d'Oglio FC", punti: 0, falli: 0 },
+  { nome: "AC Quassolo", punti: 0, falli: 0 },
+  { nome: "AS Ferralpisalo", punti: 0, falli: 0 },
+  { nome: "FC Quargnento", punti: 0, falli: 0 },
+];
+
+function numeroCasuale(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for (let i = 0; i < squadre.length; i++) {
+  squadre[i].punti = numeroCasuale(0, 100);
+  squadre[i].falli = numeroCasuale(0, 50);
+}
+
+console.log(squadre);
+
